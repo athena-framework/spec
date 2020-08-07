34336cc011c83d89bc7cda12284ef69450509f4a
George Dietrich <george@dietrich.app>
2020-08-06 21:21:54 -0400

    Add documentation and examples
    Add test methods
    Decided `Test` annotation is kinda useless
    Move annotations into the `TestCase` namespace

--- src/athena-spec.cr
+++ src/athena-spec.cr
@@ -1,8 +1,6 @@
 # Convenience alias to make referencing `Athena::Spec` types easier.
 alias ASPEC = Athena::Spec
 
-require "spec"
-
 require "./methods"
 require "./test_case"
 
@@ -10,46 +8,9 @@ require "./test_case"
 module Athena::Spec
   VERSION = "0.1.0"
 
-  #
-  annotation Test; end
-  annotation Tags; end
-  annotation Focus; end
-  annotation Pending; end
-  annotation DataProvider; end
-end
-
-struct AddSpec < Athena::Spec::TestCase
-  @value : Int32
-
-  # Initialize method is executed before every test to reset the state
-  def initialize : Nil
-    @value = 1
-  end
-
-  test "with odd value" do
-    @value.should eq 2
-  end
-
-  # Any method starting with `test_` is picked up as a test
-  def test_a : Nil
-    @value.should eq 1
-  end
-
-  # Custom name of a method using the annotation
-  @[ASPEC::Test]
-  def custom_name : Nil
-    @value += 1
-
-    @value.should eq 2
-  end
-
-  def test_b : Nil
-    @value.should eq 1
-  end
-
-  def test_with_unequal_value : Nil
-    @value.should eq 3
+  def self.run_all : Nil
+    {% for unit_test in ASPEC::TestCase.all_subclasses.reject &.abstract? %}
+      {{unit_test.id}}.run
+    {% end %}
   end
 end
-
-AddSpec.run
