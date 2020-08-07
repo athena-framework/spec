b7601f70ae1c92850e3c390ce39308d3c4c1f0ed
George Dietrich <george@dietrich.app>
2020-08-06 21:47:29 -0400

    Final touches

--- src/athena-spec.cr
+++ src/athena-spec.cr
@@ -6,8 +6,9 @@ require "./test_case"
 
 # A set of common [Spec](https://crystal-lang.org/api/Spec.html) compliant testing utilities/types.
 module Athena::Spec
-  VERSION = "0.1.0"
-
+  # Runs all `ASPEC::TestCase`s.
+  #
+  # Is equivalent to manually calling `.run` on each test case.
   def self.run_all : Nil
     {% for unit_test in ASPEC::TestCase.all_subclasses.reject &.abstract? %}
       {{unit_test.id}}.run
